d06d3a2f1445a4d0f7729f760d62b36456096b85
Qusai <qusimoh99@gmail.com>
2025-05-27 15:09:10 +0300

    fix genre

--- All_HTML/get_genres.php
+++ All_HTML/get_genres.php
@@ -1,22 +1,28 @@
 <?php
 include '../connection.php'; // ensure this file sets up $conn
-
+session_start();
+  $user_id = $_SESSION['user_id'];
+if (!isset($user_id)) {
+    http_response_code(403);
+    echo json_encode(['error' => 'User not authenticated']);
+    exit;}
 $sql = "
 SELECT DISTINCT genre FROM (
-  SELECT genre1 AS genre FROM user_genres
+  SELECT genre1 AS genre FROM user_genres WHERE user_id = $user_id
   UNION
-  SELECT genre2 FROM user_genres
+  SELECT genre2 FROM user_genres WHERE user_id = $user_id
   UNION
-  SELECT genre3 FROM user_genres
+  SELECT genre3 FROM user_genres WHERE user_id = $user_id
   UNION
-  SELECT genre4 FROM user_genres
+  SELECT genre4 FROM user_genres WHERE user_id = $user_id
   UNION
-  SELECT genre5 FROM user_genres
+  SELECT genre5 FROM user_genres WHERE user_id = $user_id
   UNION
-  SELECT genre6 FROM user_genres
+  SELECT genre6 FROM user_genres WHERE user_id = $user_id
 ) AS all_genres
 WHERE genre IS NOT NULL AND genre != ''
 LIMIT 6;
+
 ";
 
 
